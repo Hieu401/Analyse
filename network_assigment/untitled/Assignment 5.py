2c2eb95edf688dcca20060259824e7b5c3aa0671
hieu <hieu1@hotmail.com>
2019-10-29 19:10:33 +0100

    multitasking commit

--- network_assigment/untitled/Assignment 5.py
+++ network_assigment/untitled/Assignment 5.py
@@ -3,75 +3,110 @@ import select
 import json
 import itertools
 
-port = 8001
+port_host = 8001
 ip_server = "145.24.222.103"
-json_file = {}
-socket_server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client2_ip = "127.0.0.1"
+client1_id = 1
+client1_ip = socket.gethostbyname(socket.gethostname())
 
 
-def client1():
-    json_file["studentnr"] = "0964758"
-    json_file["classname"] = "INF2C"
-    json_file["clientid"] = 1
-    json_file["teamname"] = "TeamUno"
-    json_file["ip"] = '127.0.0.1'
-    json_file["secret"] = None
-    json_file["status"] = None
-    socket_server.connect((ip_server, port))
-    print(socket_server.recv(500).decode("utf-8"))
-    socket_server.send(bytes(json.dumps(json_file), "utf-8"))
-    print("message sent")
+class Client:
 
-    response_server = socket_server.recv(1024).decode("utf-8")
-    print(response_server.decode("utf-8"))
-    socket_server.close()
+    def __init__(self, client_id, message):
+        self.client_id = client_id
+        self.client_ip = socket.gethostbyname(socket.gethostname())
+        self.message = message
 
-    socket_target_client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    socket_target_client.connect(("127.0.0.1", port))
-    socket_target_client.send(json.dumps(response_server))
+    def alter_message(self, new_message):
+        self.message = new_message
+
+    def alter_json(self, id_client, ip):
+        self.message["classname"] = "INF2C"
+        self.message["clientid"] = id_client
+        self.message["teamname"] = "TeamUno"
+        self.message["ip"] = ip
+        self.message["secret"] = None
+        self.message["status"] = None
+        return self.message
+
+    def send_to(self, socket_target, ip, port):
+
+        def send_to_target():
+            # connect to target and receive welcoming message
+            socket_target.connect((ip, port))
+            print(socket_target.recv(500).decode("utf-8"))
+
+            # send to target
+            socket_target.send(bytes(json.dumps(self.message), "utf-8"))
+            print("message sent")
+
+            # receive
+            response_server = socket_target.recv(1024).decode("utf-8")
+            print(response_server)
+            socket_target.close()
+            return response_server
+
+        return send_to_target
 
 
-def client2():
-    # own server that listens for client1
-    socket_server.bind(('', port))
-    socket_server.listen(5)
-    # all inputs including inputs from server
-    inputs = [socket_server]
-    # list of all ip addresses
-    client_ip_list = []
-    while True:
-        # unify all inputs and outputs and make the program not wait for input or output
-        inputs_outputs = select.select(inputs, [], [])
-        to_process_list = list(itertools.chain(*inputs_outputs))
-        # iterate through all inputs and outputs
-        for io in to_process_list:
-            # if it is the turn of the server, accept possible connection requests
-            if io == socket_server:
-                client_socket, client_address = socket_server.accept()
+this_client = Client(1, {})
+
+# own server that listens for client1
+socket_client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+socket_client.bind((client1_ip, port_host))
+socket_client.listen(5)
+
+# all inputs including inputs from server
+inputs = [socket_client]
+
+# list of all ip addresses
+client_ip_list = []
+
+while True:
+
+    # unify all inputs and outputs and make the program not wait for input or output
+    inputs_outputs = select.select(inputs, [], [])
+    to_process_list = list(itertools.chain(*inputs_outputs))
+
+    # iterate through all inputs and outputs
+    for io in to_process_list:
+        # if it is the turn of the server, accept possible connection requests
+        if io == socket_client:
+            client_socket, client_address = socket_client.accept()
+            if client_socket is not None:
                 inputs.append(client_socket)
                 client_ip_list.append(client_address)
-            # if it is not the turn of the server, then it is a client
-            # receive the message (json string format), turn it into a dictionary and change the message
-            # then send it to the main server
+
+        # if it is not the turn of the server, then it is a client
+        # receive the message (json string format), turn it into a dictionary and change the message
+        # then send it to the main server
+        else:
+            data = io.recv(1024).decode("utf-8")
+            io.send(bytes("Client 2 says: Message received", "utf-8"))
+            json_to_dict = json.loads(data)
+            if json_to_dict["status"] == "waiting for message 2":
+                this_client.id = 2
+                json_to_dict["clientid"] = this_client.id
+                json_to_dict["ip"] = socket.gethostbyname(socket.gethostname())
+                this_client.alter_message(json_to_dict)
             else:
-                data = socket_server.recv(1024)
-                json_to_dict = json.loads(data)["secret"]
-                if json_to_dict is not None:
-                    json_to_dict["clientid"] = 2
-                    json_to_dict["ip"] = client_ip_list[0]
-                    socket_server.connect((ip_server, port))
-                    socket_server.recv(500)
-                    socket_server.send(bytes(json.dumps(json_to_dict), encoding='utf-8'))
-                    print(socket_server.recv(1024))
-                else:
-                    inputs.remove(io)
-                    io.close()
+                inputs.remove(io)
+                io.close()
 
+    # client socket
+    # client1 alter json
+    send_message = bool(int(input("Press 1 if you want to send a message, 0 otherwise")))
+    if this_client.message.len() == 0 and send_message:
+        this_client.alter_json(client1_id, client1_ip)
+        socket_server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        response = this_client.send_to(socket_server, ip_server, port_host)()
+        this_client.alter_message(json.loads(response))
 
-client = input("This program needs 2 computers, but runs the same programs. Select client 1 or client2.")
-if client == "1":
-    client1()
-elif client == "2":
-    client2()
-else:
-    print("Invalid input, please rerun the program and try again.")
+        socket_client2 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        response = this_client.send_to(socket_client2, client2_ip, port_host)()
+
+    elif this_client.message["status"] == "waiting for message 2" and send_message:
+
+        socket_server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        response = this_client.send_to(socket_server, ip_server, port_host)()
+        this_client.alter_message(json.loads(response))
